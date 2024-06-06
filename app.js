573887afdfb8c7863b71bb8d558bba03f6af98ab
ryan wetmore <ryanwetmore1014@gmil.com>
2024-06-06 11:38:26 -0400

    completed JavaScript intro exercise

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+var age;
+
 // Exercise 2
 
+const firstName = "Ryan";
+lastName = "Wetmore";
+age = 22;
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+
+let  title = "Name of the wind";
+let author = "Patrick rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
